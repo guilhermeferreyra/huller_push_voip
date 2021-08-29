0a0dd22a2941c1119e1211397d1f266261a64721
Guilherme Ferreira <barret.gui@hotmail.com>
2021-08-29 16:06:27 -0300

    ajuste na api do redis

--- redis_api/set_token.php
+++ redis_api/set_token.php
@@ -10,15 +10,19 @@ if(strcasecmp($contentType, 'application/json') != 0){
  
 $content = trim(file_get_contents("php://input"));
 $decoded = json_decode($content);
+$token = $decoded->token;
+$device = $decoded->device;
+$id_user = $decoded->id_user;
 
-function post_data($key, $value){
+function post_data($token, $device, $id_user){
     try{
         $redis = new Redis();
         $redis->connect("redis");
-        $redis->setex($key, 120, $value);
-        echo "OK!";
-    }catch( Exception $e ){ 
-        echo $e->getMessage(); 
+        $redis->set("token_$id_user", "{token:$token, device:$device}");
+        echo "token_$id_user, {token:$token, device:$device}";
+        #echo "OK!";
+    }catch( Exception $e ){
+        echo $e->getMessage();
     }
 }
 post_data($decoded->key, $decoded->value);
